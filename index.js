ae056d52a22cfbc9d48926663e10788111c51ff7
Pablo Canelos <pabloccanelos.contacto@gmail.com>
2024-11-10 22:21:31 -0300

    se carga js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const precio = 400000;
+let precioInicial = document.querySelector('.precio-inicial');
+precioInicial.innerHTML = precio;
+
+const suma = document.querySelector('#suma');
+const resta = document.querySelector('#resta');
+let cantidad = document.querySelector('.cantidad');
+let total = document.querySelector('.valor-total');
