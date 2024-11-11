c786507dc5f57e091a088f9974a4870551e2255c
Pablo Canelos <pabloccanelos.contacto@gmail.com>
2024-11-10 22:23:57 -0300

    se carga let

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ const suma = document.querySelector('#suma');
 const resta = document.querySelector('#resta');
 let cantidad = document.querySelector('.cantidad');
 let total = document.querySelector('.valor-total');
+
+let cantidadProductos = 0;
+cantidad.innerHTML = cantidadProductos;
